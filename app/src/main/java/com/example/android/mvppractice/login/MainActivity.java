f91a341dc097b6071303b17606fe497c5d2d7ab5
mihaelaplestiu <mihaela_plestiu@yahoo.com>
2018-10-24 10:52:57 +0300

    fourth  commit

--- app/src/main/java/com/example/android/mvppractice/login/MainActivity.java
+++ app/src/main/java/com/example/android/mvppractice/login/MainActivity.java
@@ -34,8 +34,6 @@ public class MainActivity extends AppCompatActivity implements LoginFragmentView
         btnCuloare.setOnClickListener(this);
         btnInviz.setOnClickListener(this);
         //getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_login, LoginFragment.newInstance()).commit();
-
-
     }
 
     private void initViews() {
